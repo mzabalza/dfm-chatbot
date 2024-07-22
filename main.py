cec521e4bb4508e7779d95c94bbee3ecc017f903
mzabalza <30298519+mzabalza@users.noreply.github.com>
2024-07-22 17:31:33 +0200

    allow image uploads

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from openai import OpenAI
+import openai
 import time
 from PIL import Image
 import os
 from dotenv import load_dotenv
+from io import BytesIO
 
 # Load environment variables from .env file
 load_dotenv()
@@ -12,9 +14,10 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 ASSISTANT_ID = os.getenv("ASSISTANT_ID")
 
-
 # Set up OpenAI client
 client = OpenAI(api_key=API_KEY)
+openai.api_key = API_KEY
+
 # Initialize session state
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -22,6 +25,11 @@ if "messages" not in st.session_state:
 if "thread_id" not in st.session_state:
     st.session_state.thread_id = None
 
+if "file_id" not in st.session_state:
+    st.session_state.file_id = None
+
+if "uploaded_file_id" not in st.session_state:
+    st.session_state.uploaded_file_id = None
 
 # Set up the Streamlit app
 st.set_page_config(page_title="AI Chat Assistant", page_icon="🤖")
@@ -32,11 +40,35 @@ st.image(logo, width=200)
 
 # Set up the Streamlit app
 st.title("Chat with AI Assistant")
+st.sidebar.title("Image uploader")
 
 
-# # Load custom avatars
-# user_avatar = Image.open("path_to_user_avatar.png")
-# bot_avatar = Image.open("path_to_bot_avatar.png")
+uploaded_file = st.sidebar.file_uploader("Upload an image file", type=["jpg", "jpeg", "png"])
+
+if (uploaded_file):
+    image = Image.open(uploaded_file)
+    st.sidebar.image(image, caption='Uploaded Image.', use_column_width=True)
+
+
+
+# If there is an uploaded file and it is different from the last uploaded file
+if uploaded_file is not None and st.session_state.uploaded_file_id != uploaded_file.file_id:
+    # we add this condition to avoid uploading the same file multiple times for each users message
+
+    # Convert PIL Image back to bytes
+    buffered = BytesIO()
+    image.save(buffered, format=image.format)
+    image_bytes = buffered.getvalue()
+
+
+    response = openai.files.create(
+        file=("imageUploadedFromMikes."+image.format.lower(), image_bytes, f"image/{image.format.lower()}"),
+        purpose='vision',
+    )
+
+    st.session_state.uploaded_file_id = uploaded_file.file_id
+    st.session_state.file_id = response.id
+
 
 # Display chat messages
 for message in st.session_state.messages:
@@ -47,6 +79,8 @@ for message in st.session_state.messages:
 user_input = st.chat_input("Type your message here...")
 
 if user_input:
+
+
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": user_input})
     with st.chat_message("user"):
@@ -57,17 +91,32 @@ if user_input:
         thread = client.beta.threads.create()
         st.session_state.thread_id = thread.id
 
+    content = [{
+        "type": "text",
+        "text": user_input
+        }
+    ]
+
+    if st.session_state.file_id:
+        content.append({
+            "type": "image_file",
+            "image_file": {
+                "file_id": st.session_state.file_id
+            }
+        })
+
     # Add user message to the thread
     client.beta.threads.messages.create(
         thread_id=st.session_state.thread_id,
         role="user",
-        content=user_input
+        content=content
     )
 
     # Run the assistant
     run = client.beta.threads.runs.create(
         thread_id=st.session_state.thread_id,
-        assistant_id=ASSISTANT_ID
+        assistant_id=ASSISTANT_ID,
+        additional_instructions="Veuillez ne pas inclure de citations ou de références sous la forme de 4:0†source】 à la fin de chaque réponse. Fournissez simplement les informations demandées sans ajouter de références explicites."
     )
 
     # Wait for the assistant to complete
@@ -78,7 +127,6 @@ if user_input:
     # Retrieve and display the assistant's response
     messages = client.beta.threads.messages.list(thread_id=st.session_state.thread_id)
     assistant_message = messages.data[0].content[0].text.value
-
     st.session_state.messages.append({"role": "assistant", "content": assistant_message})
     with st.chat_message("assistant"):
         st.write(assistant_message)
